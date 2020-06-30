bfbfc648079f34ea53cc3483a1f25a5a2e82f9c3
“DonatelloTHM” <donat.pllana@outlook.com>
2020-06-30 17:46:36 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Mr.Chocolate",7,"male","brown","choleric",true);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Rowdy",4,"male","brown","phlegmatic",true);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Tabitha",2,"female","black","choleric",false);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Sergeant Brown",5,"male","brown","choleric",true);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Melissa",6,"female","brown","melancholic",true);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Grinch",8,"female","black","seguine",false);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Wendy",3,"male","black","sanguine",true);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL,2,"male","brown","choleric",true);
