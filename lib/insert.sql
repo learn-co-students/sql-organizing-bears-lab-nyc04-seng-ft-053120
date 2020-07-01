77410c5c7206a19823f32b8c9b9bd228d02690f3
Fraciscv <Francisco.Andaur@icloud.com>
2020-06-30 22:29:56 -0400

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears VALUES (1, null, 20, "M", "dark brown", "calm", 1);
-INSERT INTO bears VALUES (2,"Agent Cooper", 10, "M", "black", "intense", 1);
-INSERT INTO bears VALUES (3,"Michelle", 6, "F", "dark brown", "Nice", 0);
-INSERT INTO bears VALUES (4,"Barbi", 19, "F", "Green", "Slimy", 1);
-INSERT INTO bears VALUES (5,"Teflon", 13, "F", "dark brown", "goofy", 1);
-INSERT INTO bears VALUES (6,"Donut", 2, "M", "Black", "Grinchy", 1);
-INSERT INTO bears VALUES (7,"Ethan", 12, "M", "Blue", "naive", 0);
-INSERT INTO bears VALUES (8,"Otha", 20, "M", "black", "aggressive", 0);
+INSERT INTO bears VALUES (1, null, 20, "M", "green", "awesome", 0);
+INSERT INTO bears VALUES (2,"Agent Cooper", 10, "M", "green", "pensive", 0);
+INSERT INTO bears VALUES (3,"Michelle", 6, "F", "blue", "nice", 0);
+INSERT INTO bears VALUES (4,"Barbi", 19, "F", "orange", "yo", 0);
+INSERT INTO bears VALUES (5,"Teflon", 13, "F", "purple brown", "none stick", 1);
+INSERT INTO bears VALUES (6,"Donut", 2, "M", "pink with chocolate sprinkles", "dad", 1);
+INSERT INTO bears VALUES (7,"Ethan", 12, "M", "clear", "happy", 1);
+INSERT INTO bears VALUES (8,"Otha", 20, "M", "black", "pensive", 0);
